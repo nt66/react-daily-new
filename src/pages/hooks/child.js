09e2320bfc04d487f8046030013e673ab5045616
y0584 <yuanm@dtdream.com>
2020-11-09 20:52:28 +0800

    ~

---
+++ src/pages/hooks/child.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+export default (props) => {
+    return (
+        <div>
+            <h1>hooks</h1>
+            <div>
+                <button>增加</button>
+                <span></span>
+            </div>
+        </div>
+    )
+}
--- src/pages/hooks/index.js
+++ src/pages/hooks/index.js
@@ -4,18 +4,25 @@ import './index.css';
 const HookTest =  (props) => {
     const [counts, setFocusIndex] = useState(0)
 
-    // useEffect使用
+    // useState使用
     const addHandler = () => {
         setFocusIndex(counts + 1)
     }
 
+    const clearHandler =() =>{
+        setFocusIndex(0)
+    }
+    
     return (
         <div>
             <h1>hooks</h1>
             <div>
-                <button onClick={() => addHandler()} >增加</button>
+                <strong> useState例子 </strong> &nbsp;
+                <button onClick={() => clearHandler()} >归零</button> &nbsp;
+                <button onClick={() => addHandler()} >增加</button> &nbsp;
                 <span>{counts}</span>
             </div>
+            
         </div>
     )
 }
