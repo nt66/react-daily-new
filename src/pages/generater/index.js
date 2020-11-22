668252ecae24901cbef5c284f90843091fa88ab6
hualingfeng <tc_hualingfeng@163.com>
2020-11-22 20:38:52 +0800

    youhua

--- src/pages/generater/index.js
+++ src/pages/generater/index.js
@@ -1,7 +1,7 @@
 // generater 示例
-export default ()=>{
+export default () => {
     // 作用1:函数流程控制
-    function* tell(){
+    function* tell() {
         yield 'a';
         yield 'b';
         return 'c';
@@ -9,31 +9,31 @@ export default ()=>{
     let k = tell();
 
     // 作用2:对象iterator化
-    let Obj = { a:1,b:2,c:3 };
-    let Ary = [1,2,3,4,5];
-    function* iterEntries(obj){
+    let Obj = { a: 1, b: 2, c: 3 };
+    let Ary = [1, 2, 3, 4, 5];
+    function* iterEntries(obj) {
         let keys = Object.keys(obj);
-        for(let i=0;i<keys.length;i++){
+        for (let i = 0; i < keys.length; i++) {
             let key = keys[i];
             let value = obj[key];
             yield [key, value]
-        }
     }
-    for(let item of iterEntries(Obj)){
-        console.log('作用2:对象iterator化:',item)
+        }
+    for (let item of iterEntries(Obj)) {
+        console.log('作用2:对象iterator化:', item)
     }
 
     // 作用3：异步操作同步化
     // 执行器
-    function fork(generator){
-        const it  = generator()
+    function fork(generator) {
+        const it = generator()
 
-        function go(result){
-            if(result.done) return result.value;
+        function go(result) {
+            if (result.done) return result.value;
 
-            return result.value.then(data=>{
+            return result.value.then(data => {
                 go(it.next(data))
-            }).catch(err=>{
+            }).catch(err => {
                 go(it.throw(err))
             })
         }
@@ -42,23 +42,23 @@ export default ()=>{
     };
 
     // promise 包装器
-    function call(url){
-        return new Promise((resolve,reject)=> {
+    function call(url) {
+        return new Promise((resolve, reject) => {
             fetch(url)
-                .then(res=>res.json())
-                .then(res=>resolve(res))
-                .catch(err=>reject(err))
+                .then(res => res.json())
+                .then(res => resolve(res))
+                .catch(err => reject(err))
         });
     }
 
-    function* fetchTopics(){
+    function* fetchTopics() {
         // const topic = yield call('http://120.27.235.113:9002/suggest/getGameArticles?page=1&game_id=1')
-        const topic = yield fetch('http://120.27.235.113:9002/suggest/getGameArticles?page=1&game_id=1').then(res=>res.json())
-        console.log('fetch1 topic:',topic.data.article[0].id);
+        const topic = yield fetch('http://120.27.235.113:9002/suggest/getGameArticles?page=1&game_id=1').then(res => res.json())
+        console.log('fetch1 topic:', topic.data.article[0].id);
         const id = topic.data.article[0].id;
         // const detail = yield call(`http://120.27.235.113:9002/suggest/articleDetail?article_id=${Number(id)}&user_id=0`)
-        const detail = yield fetch(`http://120.27.235.113:9002/suggest/articleDetail?article_id=${Number(id)}&user_id=0`).then(res=>res.json())
-        console.log('fetch2 detail:',detail);
+        const detail = yield fetch(`http://120.27.235.113:9002/suggest/articleDetail?article_id=${Number(id)}&user_id=0`).then(res => res.json())
+        console.log('fetch2 detail:', detail);
     }
 
     fork(fetchTopics)
@@ -77,7 +77,7 @@ export default ()=>{
             </div>
             <div>
                 <h2>遍历器</h2>
-                
+
             </div>
         </>
     )
